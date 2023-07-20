f19895364963dba8fd4ee5c98cf108cedfaeccd8
windersonvi <windersonvo@gmail.com>
2023-07-20 01:06:10 -0400

    first commit

---
+++ taller_js/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from "./components/header/header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ taller_js/src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 className="header_logo">Taller John Doe & Doe</h1>
+        </div>
+    );
+}
+
+export default Header
